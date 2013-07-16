6014efcfd503a630e1d8900747e77dcac78dd60f
Don Rowe <rowedonalde@gmail.com>
2013-07-16 10:31:06 -0700

    Fixed docstring format in iso_to_date

--- goodiebag.py
+++ goodiebag.py
@@ -1,13 +1,14 @@
 import datetime
 
 def iso_to_date(date):
-    '''
-    Takes a date in ISO 8601 YYYY-MM-DD format and returns the
+    """
+    Take a date in ISO 8601 YYYY-MM-DD format and return the
     equivalent Python datetime.date object.
     
     Requires:
     datetime
-    '''
+    
+    """
     date_chunks = date.split('-')
     return datetime.date(int(date_chunks[0]), int(date_chunks[1]),
                          int(date_chunks[2]))
